d6f2936f66aa32f96b69a42a7972d3665c1c31cc
Beaeze <ddong9275@naver.com>
2024-11-08 20:55:56 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int i, n, dan;
+	
+	scanf("%d", &dan);
+	
+	for (i = 1; i <= dan; i++)
+	{
+		for (n = dan; n > i; n--)
+			printf(" ");
+		for (n = 1; n <= i; n++)
+			printf("*");
+		printf("\n");
+	}
+	
+	return 0;
+}
