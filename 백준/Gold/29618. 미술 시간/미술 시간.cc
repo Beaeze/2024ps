8303602868a8651c11d062b65f650682bf51faef
Kwak, Suna <ddong9275@naver.com>
2025-03-31 15:26:11 +0900

    [Gold III] Title: 미술 시간, Time: 40 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/29618. 미술 시간/미술 시간.cc
+++ 백준/Gold/29618. 미술 시간/미술 시간.cc
@@ -9,7 +9,7 @@
 
 using namespace std;
 //미술시간은 색칠한 집합과 색칠하지 않은 집합이 존재함
-void uni(int a,int b); // 대장끼리 합칠거
+ // 대장끼리 합칠거
 int color[100001]={0}; //색칠한 색깔을 대빵이 한테만 넣어줌
 int jump[100001]={0}; //색칠한 곳 범위에 마지막 꼬리부분의 값을 넣어줌
 int x;
@@ -20,18 +20,24 @@ int main() {
 
 	int n,m; //n : 칸의 개수, m : 횟수수
 	int a, b;
-	for(int i=1;i<=100001;i++)
-		jump[i]=i+1;
     cin>>n>>m;
-	for (int i = 1; i <= m; i++) {
+	for (int i = 0; i < m; i++) {
 		cin >> a>>b>>x;
-		while(a<=b){
-			if(color[a]==0)
-				color[a]=x;
-			int next=jump[a];
-			jump[a]=b+1;
-			a=next;
+		
+    while(a<=b){
+		if(jump[a]){
+            int next=jump[a];
+            jump[a]=b+1;
+            a=next;
+			continue;
 		}
+		color[a]=x;
+		jump[a]=b+1;
+		a++;
+	}
+	for(int i=a;i<=b;i++){
+		jump[i]=b;
+    }
 	}
 	for(int i=1;i<=n;i++){
 		cout<<color[i]<<' ';
@@ -39,3 +45,4 @@ int main() {
 	
 	return 0;
 }
+
