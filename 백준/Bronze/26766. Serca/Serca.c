e2b6c3fd89a6bcc536dbec99547e83e951cd4a98
Kwak, Suna <ddong9275@naver.com>
2025-04-26 16:36:57 +0900

    [Bronze V] Title: Serca, Time: 76 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        printf(" @@@   @@@ \n@   @ @   @\n@    @    @\n@         @\n @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     ");
+        printf("\n");
+    }
+    return 0;
+}
