cbc5dee7ebe8453be1ae6875d793a5dcabbc076e
Kwak, Suna <ddong9275@naver.com>
2025-09-24 20:27:59 +0900

    [Silver III] Title: N과 M (5), Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<limits.h>
+#include<tuple>
+
+using namespace std;
+
+int n,m;
+vector<int>re; //지금거쳐가고 있는 곳
+bool v[9]; //방문확인
+int arr[9];
+
+void back(int deep){
+    if(deep==m){
+        for(auto i: re)cout<<i<<' ';
+        cout<<"\n";
+        return;
+    }
+    for(int i=1;i<=n;i++){
+        if(v[i])continue;
+        v[i]=true;
+        re.push_back(arr[i]);
+        back(deep+1);//이거 deep++하면 안됨
+        //경우의 수를 출력한 후 함수를 빠져나온 후
+        v[i]=false;
+        re.pop_back();
+    }
+}
+
+int main(){
+
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    cin>>n>>m;
+
+    for(int i=1;i<=n;i++)cin>>arr[i];
+
+    sort(arr+1,arr+n+1);
+
+    back(0);
+
+	return 0;
+
+}
